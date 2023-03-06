8078c6ae244a9391104bd09f2f3112f7207e6412
HyungJinHan <han1210_36@naver.com>
2023-03-06 15:54:50 +0900

    예제 데이터 불러오기 성공

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ exploreData }: any) => {
   return (
     <div className="selection:text-white selection:bg-[#FF5A5F]">
       <Head>
@@ -13,8 +13,32 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints */}
+          {exploreData.map((item: any) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    // 기존 url인 "https://links.papareact.com/pyp"로 하게 되면 fetch 에러
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
+}
