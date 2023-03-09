1b321e5db0d893d1c449c97d69adf59a5c541a78
HyungJinHan <han1210_36@naver.com>
2023-03-09 15:59:28 +0900

    search 결과 페이지 스타일링 및 버튼 구현 중

--- pages/search.tsx
+++ pages/search.tsx
@@ -9,7 +9,21 @@ const search = (props: Props) => {
     <div>
       <Header />
 
-      <main className="flex"></main>
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+
+          <h1 className="text-3xl font-semibold mb-6">Stays in Mars</h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="searchPageButton">Cancellation Flexibility</p>
+            <p className="searchPageButton">Type of place</p>
+            <p className="searchPageButton">Price</p>
+            <p className="searchPageButton">Rooms and Beds</p>
+            <p className="searchPageButton">More filters</p>
+          </div>
+        </section>
+      </main>
 
       <Footer />
     </div>
