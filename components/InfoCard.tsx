933c2e2d3e7154296e707851e058c0fcfdc7aa20
HyungJinHan <han1210_36@naver.com>
2023-03-10 15:43:02 +0900

    검색 결과 데이터 연동 완료 및 스타일링 진행 중

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,3 +1,5 @@
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
 
@@ -21,9 +23,19 @@ const InfoCard = ({
   total,
 }: Props) => {
   return (
-    <div>
+    <div className="flex">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
         <Image src={img} alt="resultImage" fill className="object-cover" />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+          {/* <StarIcon /> */}
+        </div>
+
+        <h4 className="text-xl">{title}</h4>
       </div>
     </div>
   );
